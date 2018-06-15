76b6fcf17f697e55d59d4b54e918610ff5fa5ae3
macuser1 <macuser1@macuser1s-MacBook-Pro.local>
2018-06-15 11:10:32 -0500

    section 3

--- app.js
+++ app.js
@@ -2,15 +2,32 @@ const express = require('express');
 const volleyball = require('volleyball');
 const bodyParser = require('body-parser');
 
+const { db, Page, User } = require('./models')
+db.authenticate().
+then(() => {
+  console.log('connected to the database');
+})
+
 const app = express();
 
 app.use(volleyball);
 
 app.use(express.static(__dirname.concat('/public')));
 
+const layout= require("./views/layout");
+
 app.get('/', (req, res) => {
-  res.send('Hello World.');
+  res.send(layout(""));
 });
 // app.use(router);
 
-app.listen(1337);
+const init = async () => {
+  await User.sync();
+  await Page.sync();
+
+  app.listen(1337, ()=> {console.log("listening")});
+
+}
+
+init();
+
